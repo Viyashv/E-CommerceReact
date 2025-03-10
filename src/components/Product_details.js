74213e107b43b7569254d844ea06f75caab112b5
Viyashv <viyashturkane@gmail.com>
2025-03-10 19:46:01 +0530

    Implement the api intergration also used hooks like useEffect, useState

--- src/components/Product_details.js
+++ src/components/Product_details.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 export default function Product_details() {
+  const [product, getProduct] = useState(null)
+  const id = useParams().id
+  async function fectProductById() {
+    let response = await fetch(`https://dummyjson.com/products/${id}`, { method: 'GET' })
+    let data = await response.json()
+    getProduct(data)
+  }
+  useEffect(() => {
+    fectProductById()
+  }, [])
   return (
+
     <div className='container'>
       <h1 className='text-center'>
         This is the Product details page for product {useParams().id}
-                            <h2>{useParams().name}</h2>
-                            <p>Price: {useParams().price}</p>
-      </h1> 
+        {useParams().name}
+        <p>Price: {useParams().price}</p>
+      </h1>
+      <div className='row'>
+        {
+          product &&
+          <div className='col-md-3 mt-3'>
+            <div className="card">
+              <img src={product.thumbnail} className="card-img-top" alt="..." loading='lazy' />
+              <div className="card-body">
+                <h5 className="card-title">Name:- {product.title.slice(0,15)}</h5>
+                <p className="card-text">Description:- {product.description.slice(0,95)}...</p>
+                <p className="card-text">Price :- <del>&#8377;{product.price}</del>&#8377;{product.discountPercentage}</p>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   )
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import { useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const products = [{name: 'Product 1', price: 100}, {name: 'Product 2', price: 200}, {name: 'Product 3', price: 300}, {name: 'Product 4', price: 400}, {name: 'Product 5', price: 500}]
+// const products = [{name: 'Product 1', price: 100}, {name: 'Product 2', price: 200}, {name: 'Product 3', price: 300}, {name: 'Product 4', price: 400}, {name: 'Product 5', price: 500}]
 export default function Products() {
+    const [products , setProducts]= useState(null)
+    async function fetchProducts() {
+        let response = await fetch('https://dummyjson.com/products' ,{ method: 'GET'})
+        let data = await response.json()
+        setProducts(data.products)
+    }
+    useEffect(() => {
+        fetchProducts()
+    },[])
   return (
     <div className='container'>
         <h1 className='text-center'>Products</h1>
         <div className='row'>
             {
-                products.map((product,index) => {
+                products && products.map((product,index) => {
                     return (
-                        <div className='col-md-3'>
-                            <h2>{product.name}</h2>
-                            <p>Price: {product.price}</p>
-                            <Link to={`/product/${index}`} className='btn btn-primary'>View</Link>
+                        <div className='col-md-3 mt-3' key={index}>
+                            <div className="card">
+                                <img src={product.thumbnail} className="card-img-top" alt="..." loading='lazy'/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title.slice(0,15)}</h5>
+                                    <p className="card-text">{product.description.slice(0,95)}...</p>
+                                    <p className="card-text">Price :- <del>&#8377;{product.price}</del> Discount:- &#8377;{product.discountPercentage}</p>
+                                    <Link to={`/product/${product.id}`} className="btn btn-primary">View more</Link>
+                                </div>
+                            </div>
                         </div>
                     )
                 })
